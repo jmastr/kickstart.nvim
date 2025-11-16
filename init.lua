9a70d481bab3f4b702d5fbb2d9733223fbb2ca80
Julian Strobl <jmastr@mailbox.org>
2025-11-16 18:11:06 +0100

    feat(init): jump to last position when reopening a file
    Signed-off-by: Julian Strobl <jmastr@mailbox.org>

--- init.lua
+++ init.lua
@@ -137,6 +137,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Jump to last position when reopening a file
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Open file at the last position it was edited earlier',
+  group = vim.api.nvim_create_augroup('last_loc', { clear = true }),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
